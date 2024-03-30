6b24e9e283249e0fa50dc822ae7f632b7aeeac47
Yunjin <jed.donaire08@gmail.com>
2024-03-30 14:05:36 +0800

    Perf: Organized the structure of the folder

---
+++ constants/index.ts
@@ -0,0 +1,27 @@
+export const sideBarLinks = [
+    {
+        label: 'Home',
+        route: '/',
+        imgUrl: '/iconse/Home.svg',
+    },
+    {
+        label: 'Upcoming',
+        route: '/upcoming',
+        imgUrl: '/iconse/upcoming.svg',
+    },
+    {
+        label: 'Previous',
+        route: '/previous',
+        imgUrl: '/iconse/previous.svg',
+    },
+    {
+        label: 'Recordings',
+        route: '/recordings',
+        imgUrl: '/iconse/Video.svg',
+    },
+    {
+        label: 'Personal Room',
+        route: '/personal-room',
+        imgUrl: '/iconse/add-personal.svg',
+    },
+]
