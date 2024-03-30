99f30931a1f2227687fecff741b7387a74618258
Yunjin <jed.donaire08@gmail.com>
2024-03-30 14:29:51 +0800

    Perf: Added Sidebar and custom Images

--- constants/index.ts
+++ constants/index.ts
@@ -2,26 +2,26 @@ export const sideBarLinks = [
     {
         label: 'Home',
         route: '/',
-        imgUrl: '/iconse/Home.svg',
+        imgUrl: '/icons/Home.svg',
     },
     {
         label: 'Upcoming',
         route: '/upcoming',
-        imgUrl: '/iconse/upcoming.svg',
+        imgUrl: '/icons/upcoming.svg',
     },
     {
         label: 'Previous',
         route: '/previous',
-        imgUrl: '/iconse/previous.svg',
+        imgUrl: '/icons/previous.svg',
     },
     {
         label: 'Recordings',
         route: '/recordings',
-        imgUrl: '/iconse/Video.svg',
+        imgUrl: '/icons/Video.svg',
     },
     {
         label: 'Personal Room',
         route: '/personal-room',
-        imgUrl: '/iconse/add-personal.svg',
+        imgUrl: '/icons/add-personal.svg',
     },
 ]
