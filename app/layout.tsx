37d25e8e840110e14a1fc7113b7719ecfc9d8f0c
Yunjin <jed.donaire08@gmail.com>
2024-03-31 00:40:54 +0800

    Perf: utilize toaster for components

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
+import { Toaster } from "@/components/ui/toaster";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,19 +21,22 @@ export default function RootLayout({
       <ClerkProvider
         appearance={{
           layout: {
-            logoImageUrl: '/icons/yoom-logo.svg',
-            socialButtonsVariant: 'iconButton'
+            logoImageUrl: "/icons/yoom-logo.svg",
+            socialButtonsVariant: "iconButton",
           },
           variables: {
-            colorText: '#fff',
-            colorPrimary: '#0E78F9',
-            colorBackground: '#1c1f2e',
-            colorInputBackground: '#252a41',
-            colorInputText: '#fff'
-          }
+            colorText: "#fff",
+            colorPrimary: "#0E78F9",
+            colorBackground: "#1c1f2e",
+            colorInputBackground: "#252a41",
+            colorInputText: "#fff",
+          },
         }}
       >
-        <body className={`${inter.className} bg-dark-2`}>{children}</body>
+        <body className={`${inter.className} bg-dark-2`}>
+          {children}
+          <Toaster />
+        </body>
       </ClerkProvider>
     </html>
   );
--- components/HomeCard.tsx
+++ components/HomeCard.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import React from "react";
 
 interface HomeCardProps {
-  className: string;
+  className?: string;
   img: string;
   title: string;
   description: string;
