b648c62738194f42aa0e356053db1499a1c0fcb0
Yunjin <jed.donaire08@gmail.com>
2024-04-01 10:11:33 +0800

    perf: make endcall card component

---
+++ components/EndCallButton.tsx
@@ -0,0 +1,31 @@
+import { useCall, useCallStateHooks } from "@stream-io/video-react-sdk";
+import { useRouter } from "next/navigation";
+import React from "react";
+import { Button } from "./ui/button";
+
+function EndCallButton() {
+  const call = useCall();
+  const router = useRouter();
+  const { useLocalParticipant } = useCallStateHooks();
+  const localParticipant = useLocalParticipant();
+
+  const isMeetingOwner =
+    localParticipant &&
+    call?.state.createdBy &&
+    localParticipant.userId === call.state.createdBy.id;
+
+  if (!isMeetingOwner) return null;
+
+  const endCall = async () => {
+    await call.endCall();
+    router.push("/");
+  };
+
+  return (
+    <Button onClick={endCall} className="bg-red-500">
+      End call for everyone
+    </Button>
+  );
+}
+
+export default EndCallButton;
