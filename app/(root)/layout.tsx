bebe13ce3c846c56ce230d601ac4fc13ec86c4ec
Yunjin <jed.donaire08@gmail.com>
2024-04-01 10:06:48 +0800

    refactor: Changed UI layout to utilize metadata

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,5 +1,14 @@
 import StreamVideoProvider from "@/providers/StreamClientProvider";
 import React, { ReactNode } from "react";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Joom",
+  description: "Video Calling App",
+  icons: {
+    icon: "/icons/logo.svg",
+  },
+};
 
 const RootLayout = ({ children }: { children: ReactNode }) => {
   return (
