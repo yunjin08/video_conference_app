ce9f648adccead0574219d50ce5a4da3103ef678
Yunjin <jed.donaire08@gmail.com>
2024-04-20 01:35:12 +0800

    chore; get real time data from upcoming

--- app/api/upcoming/route.js
+++ app/api/upcoming/route.js
@@ -28,3 +28,27 @@ export const POST = async (request) =>  {
       }
     } 
 }
+
+export const GET = async (request) =>  {
+  if (request.method === 'GET') {
+    try {
+      
+      const now = new Date();
+      const upcomingMeeting = await prisma.UpcomingMeeting.findFirst({
+        where: {
+          meeting_time: {
+            gt: now, // greater than the current time
+          },
+        },
+        orderBy: {
+          meeting_time: 'asc', // ensures the closest future meeting is selected
+        },
+      });
+    
+      return new Response(JSON.stringify(upcomingMeeting), { status: 200, headers: { 'Content-Type': 'application/json' } });
+    } catch (error) {
+      console.error('Error reading the database:', error);
+      return new Response("Error reading the database", { status: 500 });
+    }
+  } 
+}
