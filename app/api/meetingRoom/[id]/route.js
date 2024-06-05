9118af99a1c45bfa8054958e8bdeb33753a10580
Yunjin <jed.donaire08@gmail.com>
2024-06-05 20:48:44 +0800

    [Modification][Jed]- Added Deletiong for created meeting rooms

--- app/api/meetingRoom/[id]/route.js
+++ app/api/meetingRoom/[id]/route.js
@@ -22,3 +22,24 @@ export const GET = async (request, { params }) => {
     }
   }
 };
+
+
+export const DELETE = async (request, { params }) => {
+  if (request.method === "DELETE") {
+    try {
+        await prisma.MeetingRooms.delete({
+          where: {
+            room_meeting : params.id,
+          },
+      });
+
+      return new Response("Deleted this room", {
+        status: 200,
+        headers: { "Content-Type": "application/json" },
+      });
+    } catch (error) {
+      console.error("Error reading the database:", error);
+      return new Response("Error reading the database", { status: 500 });
+    }
+  }
+};
