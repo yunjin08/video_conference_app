d8cb825da0fec56ea69269aab172c66d4bfe4a14
Yunjin <jed.donaire08@gmail.com>
2024-06-05 20:47:39 +0800

    [Refactor][Jed]- Changed data call for getting participants

--- app/api/meeting/[id]/route.js
+++ app/api/meeting/[id]/route.js
@@ -1,39 +1,19 @@
 import prisma from "@/lib/prisma";
 
-export const DELETE = async (request, { params }) => {
-    if (request.method === "DELETE") {
-      console.log(params.id, 'params');
-      try {
-          await prisma.MeetingDetails.delete({
-          where: {
-            meeting_id: params.id,
-          },
-        });
-  
-        return new Response("Deleted this recording", {
-          status: 200,
-          headers: { "Content-Type": "application/json" },
-        });
-      } catch (error) {
-        console.error("Error reading the database:", error);
-        return new Response("Error reading the database", { status: 500 });
-      }
-    }
-  };
-
   export const GET = async (request, {params}) => {
     if (request.method === "GET") {
       try {
-        const MeetingDetails = await prisma.MeetingDetails.findMany({
+        const MeetingDetails = await prisma.Participants.findMany({
           where: {
-            OR: [
-              { creator_user_id: params.id },
-              { participants: { some: { participant_id: params.id } } }
-            ]
+            participant_id: params.id,
           },
           include: {
-            creator: true, // Include related creator details
-            recordings: true // Include related recordings
+            meeting: {
+              include: {
+                creator: true, // Include related creator details
+                recordings: true // Include related recordings
+              }
+            } // Include related creator details
           }
         });
         return new Response(JSON.stringify(MeetingDetails), {
